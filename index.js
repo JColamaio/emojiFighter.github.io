b3e4435994f69395924961830a41dcff489bbbaa
JColamaio <jcolamaio@gmail.com>
2022-06-26 20:38:59 -0300

    structure

---
+++ index.js
@@ -0,0 +1,8 @@
+const fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+const stageEl = document.getElementById("stage")
+const fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+  
+})
