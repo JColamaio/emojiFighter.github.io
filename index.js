8ffa78dd983796138fb581bf3e496ecd92420ece
JColamaio <jcolamaio@gmail.com>
2022-06-26 20:48:12 -0300

    final commit

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const stageEl = document.getElementById("stage")
 const fightButton = document.getElementById("fightButton")
 
 fightButton.addEventListener("click", function() {
-  
+  const randomIndexOne = Math.floor(Math.random() * fighters.length)
+  const randomIndexTwo = Math.floor(Math.random() * fighters.length)
+  stageEl.textContent = `${fighters[randomIndexOne]} vs ${fighters[randomIndexTwo]}`
 })
